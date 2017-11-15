d86d27c718159a61c4a82ff3611765a9b4b5a310
HienTran <hien.tran@dounets.com>
2017-11-15 17:03:55 +0700

    add something

--- src/plugin/vessel-structure/const.js
+++ src/plugin/vessel-structure/const.js
@@ -1,10 +1,13 @@
 export const PLUGIN_NAME = "vesselStructure";
 export const OPTIONS_DEFAULT = {
-  width: 1300,
+  width: 1000,
   height: 800,
   bgColor: "#ffffff",
+  vslImages: {
+    body: 'assets/images/ship_body-r.gif',
+    bridge: 'assets/images/top_2r.gif',
+  }
 }
-
 export const LIST_CONTROLS = {
 
 }
--- src/plugin/vessel-structure/vessel-structure.js
+++ src/plugin/vessel-structure/vessel-structure.js
@@ -55,11 +55,103 @@ class Plugin {
   }
 
   renderVesselView() {
-    let vslImg = $('<div id="vsl-img">');
-    vslImg.css({
-      'background-image' : "url('assets/images/ship_engine.png')"
-    })
-    this.sideView.append(vslImg);
+    console.log("plugin data: ", this.pluginData);
+
+    let {hatchs} = this.pluginData;
+    let {vslImages} = this.options;
+    console.log("vslImages: ", vslImages);
+    let $vslContent = $(`<div class="vsl-content"></div>`);
+
+    let vslImgBody = $(`<img id="vsl-img-body" src="${vslImages.body}">`);
+    let vslImgBridge = $(`<img id="vsl-img-bridge" src="${vslImages.bridge}">`);
+
+    let partitionNm = $(`<div class="partition-nm"></div>`);
+
+    $vslContent.append(vslImgBody);
+    $vslContent.append(vslImgBridge);
+    $vslContent.append(partitionNm);
+
+    let html = `<div class="ship-crane"><span>C</span></div>`;
+    if(hatchs){
+      for(let i=0; i<hatchs.length; i++){
+
+      }
+    }
+
+    $vslContent.append(html);
+
+    this.sideView.append($vslContent);
+  }
+
+  renderPartitionView(){
+    let {icons} = this.options;
+
+    let $vslContent = $(`<div class="partition-content"></div>`);
+    let $detail = $(`<div class="partition-detail"></div>`);
+
+    let $lable = $(`<div class="partition-label"></div>`);
+    let lblRowhtml = `
+      <div class="lbl-left"><span>Row</span></div>
+      <div class="lbl-center"></div>
+      <div class="lbl-right"><span>Deck</span></div>
+    `;
+    $lable.append(lblRowhtml);
+    $detail.append($lable);
+
+    let $grid = $(`<div class="partition-grid"></div>`);
+    let $leftCol = $(`<div class="left-col"></div>`);
+    let $rightCol = $(`<div class="right-col"></div>`);
+    $grid.append($leftCol);
+    $grid.append($rightCol);
+
+    //Build left col
+    let totalDeck = 4;
+    let rowHright = 100 / totalDeck;
+
+    for(let i=0; i<totalDeck;i++) {
+      let $row = $(`<div class="grid-row"></div>`);
+      $row.css({
+        "height" : `${rowHright}%`
+      });
+
+      let tableStyle = `
+        border-top: ${i != totalDeck ? '2px solid' : 'none'};
+        border-top-color: ${(i != 0 && i != totalDeck) ? '#d375f7' : '#000'};
+        border-bottom: ${i == totalDeck - 1 ? '2px solid' : 'none'}
+      `;
+
+      let colHtml = `
+      <div class="left-tools">
+        <a href="javascript:;" class="btn-slot-plus">
+          <i class="fa fa-plus-square-o" aria-hidden="true"></i>
+        </a>
+        <span class="slot-name">${i}</span>
+        <a href="javascript:;" class="btn-slot-sub">
+          <i class="fa fa-minus-square-o" aria-hidden="true"></i>
+        </a>
+      </div>
+      <div class="partition-table" style="${tableStyle}"></div>
+    `;
+      $row.append(colHtml);
+      $leftCol.append($row);
+    }
+
+    //Build right col
+    let rightColHtml = `
+      <a href="javascript:;" class="btn-deck-plus">
+        <i class="fa fa-plus-square-o" aria-hidden="true"></i>
+      </a>
+      <span class="deck-name">1</span>
+      <a href="javascript:;" class="btn-deck-sub">
+        <i class="fa fa-minus-square-o" aria-hidden="true"></i>
+      </a>
+    `;
+    $rightCol.append(rightColHtml);
+    $detail.append($grid);
+
+    $vslContent.append($detail);
+
+    this.partitionView.append($vslContent);
   }
 
   render() {
@@ -76,7 +168,8 @@ class Plugin {
     this.renderVesselView();
     this.$element.append(this.sideView);
 
-    this.partitionView = this.template.partitionView;
+    this.partitionView = $(this.template.partitionView);
+    this.renderPartitionView();
     this.$element.append(this.partitionView);
   }
 
